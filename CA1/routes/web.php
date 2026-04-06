5eafee366bf1c3c5f10f92290334d5ba643f0461
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-04-06 16:46:07 +0530

    Task 2 Done

--- CA1/routes/web.php
+++ CA1/routes/web.php
@@ -7,3 +7,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/show/{name}' ,function($name) {
     return view('info' , compact('name'));
 });
+
+Route::view('/adf/sdf' , 'info')->name('dhruva');
+Route::view('/' , 'welcome');
+
+
+// Task 2
+Route::get('/id/{id}' , function($id){
+    if($id == 1){
+        return response()->JSON([
+        "name" => "Dhruva",
+        "Role" => "Student",
+        ]);
+    }
+    else{
+        return redirect('/error');
+    }
+});
+Route::get('/error' , function(){
+    return 'Invalid user';
+});
