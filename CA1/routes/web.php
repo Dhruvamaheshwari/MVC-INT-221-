85d0ddd73ac284d71567074be7945e3b2fe1fcc1
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-04-06 15:52:24 +0530

    Task 1 Done

--- CA1/routes/web.php
+++ CA1/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Task 1
+Route::get('/show/{name}' ,function($name) {
+    return view('info' , compact('name'));
 });
